ad460cef826c23b3653ad5e7acff41ab7a859438
furadWho <1337543811@qq.com>
2021-11-30 16:18:00 +0800

    2021-11-30 add some files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # goDocker
 基于Go Docker SDK 操作 Docker 
+
+# 阐述
+基于　go docker sdk 实现一部分工具去直接使用代码操作　Docker (仅仅只是用一下)
--- cmd/goDocker/main.go
+++ cmd/goDocker/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	log "github.com/sirupsen/logrus"
+)
 
 func main()  {
-	fmt.Println("helo")
+	ctx := context.Background()
+
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Errorf("NewClientWithOpts ERRO : %s \n",err)
+		return
+	}
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		log.Errorf("ContainerList ERRO : %s \n",err)
+		return
+	}
+
+	for _, container := range containers {
+		fmt.Println(container.ID)
+	}
+
+
+	images, err := cli.ImageList(ctx, types.ImageListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, image := range images {
+		fmt.Println(image.ID)
+	}
+
 }
